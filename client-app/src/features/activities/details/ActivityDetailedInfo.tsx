c6502112c39f57a9aee6d2c554936b007f9f67f9
Victor Lau <victor.lau@anvione.com>
2020-11-27 16:49:57 +0800

    End of Section 11

--- client-app/src/features/activities/details/ActivityDetailedInfo.tsx
+++ client-app/src/features/activities/details/ActivityDetailedInfo.tsx
@@ -1,3 +1,4 @@
+import { format } from 'date-fns'
 import React from 'react'
 import { Segment, Grid, Icon } from 'semantic-ui-react'
 import { IActivity } from '../../../app/models/activity'
@@ -22,7 +23,7 @@ const ActivityDetailedInfo: React.FC<{activity: IActivity}> = ({activity}) => {
                   </Grid.Column>
                   <Grid.Column width={15}>
                     <span>
-                      {activity.date}
+                      {format(activity.date,'eeee do MMMM')} at {format(activity.date,'h:mm a')}
                     </span>
                   </Grid.Column>
                 </Grid>
