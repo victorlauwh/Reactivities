f8f338680ce9e508e60179f4f70d13ec16bb74ce
Victor Lau <victor.lau@anvione.com>
2021-03-15 17:53:39 +0800

    End of Section 17

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -3,16 +3,20 @@ import { IActivity } from '../models/activity';
 import { history } from '../..';
 import { toast } from 'react-toastify';
 import { IUser, IUserFormValues } from '../models/user';
+import { IPhoto, IProfile } from '../models/profile';
 
 axios.defaults.baseURL = 'http://localhost:5000/api';
 
-axios.interceptors.request.use((config) => {
-  const token = window.localStorage.getItem('jwt');
-  if(token) config.headers.Authorization = `Bearer ${token}`;
-  return config
-}, error =>{
-  return Promise.reject(error);
-})
+axios.interceptors.request.use(
+  (config) => {
+    const token = window.localStorage.getItem('jwt');
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 axios.interceptors.response.use(undefined, (error) => {
   if (error.message === 'Network Error' && !error.response) {
@@ -51,6 +55,15 @@ const requests = {
   put: (url: string, body: {}) =>
     axios.put(url, body).then(sleep(1000)).then(responseBody),
   del: (url: string) => axios.delete(url).then(sleep(1000)).then(responseBody),
+  postForm: (url: string, file: Blob) => {
+    let formData = new FormData();
+    formData.append('File', file);
+    return axios
+      .post(url, formData, {
+        headers: { 'Content-type': 'multipart/form-data' },
+      })
+      .then(responseBody);
+  },
 };
 
 const Activities = {
@@ -60,17 +73,29 @@ const Activities = {
   update: (activity: IActivity) =>
     requests.put(`/activities/${activity.id}`, activity),
   delete: (id: string) => requests.del(`/activities/${id}`),
-  attend: (id: string) => requests.post(`/activities/${id}/attend`,{}),
-  unattend: (id: string) => requests.del(`/activities/${id}/attend`)
+  attend: (id: string) => requests.post(`/activities/${id}/attend`, {}),
+  unattend: (id: string) => requests.del(`/activities/${id}/attend`),
 };
 
 const User = {
   current: (): Promise<IUser> => requests.get('/user'),
-  login: (user: IUserFormValues): Promise<IUser> => requests.post('/user/login', user),
-  register: (user: IUserFormValues): Promise<IUser> => requests.post('/user/register', user),
-}
+  login: (user: IUserFormValues): Promise<IUser> =>
+    requests.post('/user/login', user),
+  register: (user: IUserFormValues): Promise<IUser> =>
+    requests.post('/user/register', user),
+};
+
+const Profiles = {
+  get: (username: string): Promise<IProfile> =>
+    requests.get(`/profiles/${username}`),
+  uploadPhoto: (photo: Blob): Promise<IPhoto> =>
+    requests.postForm(`/photos`, photo),
+    setMainPhoto: (id: string) => requests.post(`/photos/${id}/setMain`, {}),
+    deletePhoto: (id: string) => requests.del(`/photos/${id}`)
+};
 
 export default {
   Activities,
-  User
+  User,
+  Profiles,
 };
---
+++ client-app/src/app/common/photoUpload/PhotoWidgetCropper.tsx
@@ -0,0 +1,40 @@
+import React, { useRef } from 'react';
+import Cropper from 'react-cropper';
+import 'cropperjs/dist/cropper.css';
+
+interface IProps {
+  setImage: (file: Blob) => void;
+  imagePreview: string;
+}
+
+const PhotoWidgetCropper: React.FC<IProps> = ({ setImage, imagePreview }) => {
+  const cropperRef = useRef<HTMLImageElement>(null);
+  const cropImage = () => {
+    const imageElement: any = cropperRef?.current;
+    const cropper: any = imageElement?.cropper;
+
+    cropper.getCroppedCanvas().toBlob((blob: any) => {
+      setImage(blob);
+    }, 'image/jpeg');
+  };
+
+  return (
+    <Cropper
+      ref={cropperRef}
+      src={imagePreview}
+      style={{ height: 200, width: '100%' }}
+      // Cropper.js options
+      aspectRatio={1 / 1}
+      preview='.img-preview'
+      guides={false}
+      viewMode={1}
+      dragMode='move'
+      scalable={true}
+      cropBoxMovable={true}
+      cropBoxResizable={true}
+      crop={cropImage}
+    />
+  );
+};
+
+export default PhotoWidgetCropper;
